201dd04462664fc6b499386482a6dba4094d810f
RubenDeLeeuw <61274077+RubenDeLeeuw@users.noreply.github.com>
2020-11-11 21:47:35 +0100

    Update volgehuisje.py

--- Python Code/volgehuisje.py
+++ Python Code/volgehuisje.py
@@ -1,1 +1,32 @@
 
+from gpiozero import MotionSensor
+from time import sleep
+from picamera import PiCamera
+pir = MotionSensor(4) #pin nr
+import datetime
+
+camera = PiCamera()
+teller = 0
+recording = 0
+
+while True:
+        while pir.motion_detected:
+                if recording == 0:
+                        i = datetime.datetime.now()
+                        camera.start_recording('/var/www/html/owncloud/vogelhuisje/video %s.h264' %i)
+                        print("Start Recording")
+                        recording = 1
+                if recording == 1:
+                        sleep(1)
+                        teller = teller + 1
+                        print(teller)
+                if teller == 300 and recording == 1:
+                        camera.stop_recording()
+                        x = datetime.datetime.now()
+                        camera.start_recording('/var/www/html/owncloud/vogelhuisje/video %s.h264' %i)
+                        teller = 0
+        if pir.motion_detected == False and recording == 1:
+                camera.stop_recording()
+                print("Stop Recording")
+                recording = 0
+                teller = 0
